ba080f2fbc33ac5df1606074f272e7f9108d4ba3
debjotimallick <debjoti.mallick@outlook.com>
2026-06-06 20:46:38 +0530

    added terraform code for vpc and subnets

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC."
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "Public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "Private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+}
